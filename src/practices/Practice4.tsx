11a62e1ff5210a6e5cf2b3beeeb09c66046b3c22
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-08-03 06:35:37 +0900

    暗黙のanyを承認する設定
    既存のプロジェクトにTypeScriptを導入する時などは便利かも！？

---
+++ src/practices/Practice4.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const Practice4 = () => {
+  const calcTotalFee = (num) => {
+    const total = num * 1.1;
+    console.log(total);
+  }
+
+  const onClickPractice = () => calcTotalFee(1000);
+
+  return (
+    <div>
+      <p>設定ファイルを触ってみる</p>
+      <button onClick={onClickPractice}>練習問題４を実行</button>
+    </div>
+  );
+};
